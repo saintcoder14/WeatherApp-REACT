ab193f7caceba1f9a9b8c555af3eb7d54ff534f9
Samridh Upadhyay <samridhu10@outlook.com>
2021-09-21 17:59:59 +0530

    Added Data Input from .js File

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import { WiDaySunny } from "react-icons/wi";
+import { WiCloud, WiDaySunny } from "react-icons/wi";
 
-
-const Card = () => {
+const Card = (props) => {
+    var t = new Date().toLocaleTimeString();
+    var d = new Date().toLocaleDateString();
     return (
         <>
             <div className="card w-77" >
-                    <div className="card-body text-center">
-                            <h4 className="card-title">Friday</h4>
-                            <p className="display-10 text-muted">March 1st,1:00 pm</p>
-                            <WiDaySunny size="4em"/>
-                            <h4>35 &deg;C</h4>
-                            <p>Clear Sky</p>
-                    </div>
+            <div className="card-body text-center">
+                    <h4 className="card-title">{props.days}</h4>
+                    <p className="display-10 text-muted">{d}, {t}</p>
+                    {
+                        props.cond==="sunny"? <WiDaySunny size="4em"/>:<WiCloud size="4em"/>
+                    }
+                    <h4>{props.tempdetails}</h4>
+                    <p>{props.cond}</p>
+            </div>
             </div>
         </>
     )
